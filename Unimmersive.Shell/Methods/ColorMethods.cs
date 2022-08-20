68b1712c279df44cbb4d1db0463140d2f196f250
Chaz "Gamerappa" Peloquin <chazpelo2@gmail.com>
2022-08-20 18:36:12 -0400

    Load list of apps and tile coloring

---
+++ Unimmersive.Shell/Methods/ColorMethods.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Drawing;
+using SDColor = System.Drawing.Color;
+using WMColor = System.Windows.Media.Color;
+
+namespace Unimmersive.Shell.Methods
+{
+    internal class ColorMethods
+    {
+        public static SDColor GetDominantColor(Bitmap bmp)
+        {
+
+            //Used for tally
+            int r = 0;
+            int g = 0;
+            int b = 0;
+
+            int total = 0;
+
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    SDColor clr = bmp.GetPixel(x, y);
+
+                    r += clr.R;
+                    g += clr.G;
+                    b += clr.B;
+
+                    total++;
+                }
+            }
+
+            //Calculate average
+            r /= total;
+            g /= total;
+            b /= total;
+
+            return SDColor.FromArgb(r, g, b);
+        }
+
+        public static SDColor ChangeColorBrightness(SDColor color, float correctionFactor)
+        {
+            float red = (float)color.R;
+            float green = (float)color.G;
+            float blue = (float)color.B;
+
+            if (correctionFactor < 0)
+            {
+                correctionFactor = 1 + correctionFactor;
+                red *= correctionFactor;
+                green *= correctionFactor;
+                blue *= correctionFactor;
+            }
+            else
+            {
+                red = (255 - red) * correctionFactor + red;
+                green = (255 - green) * correctionFactor + green;
+                blue = (255 - blue) * correctionFactor + blue;
+            }
+
+            return SDColor.FromArgb(color.A, (int)red, (int)green, (int)blue);
+        }
+
+        public static SDColor IdealTextColor(SDColor bg)
+        {
+            int nThreshold = 105;
+            int bgDelta = Convert.ToInt32((bg.R * 0.299) + (bg.G * 0.587) +
+                                          (bg.B * 0.114));
+
+            SDColor foreColor = (255 - bgDelta < nThreshold) ? SDColor.Black : SDColor.White;
+            return foreColor;
+        }
+
+        public static WMColor SDColorToWMColor(SDColor color)
+        {
+            return WMColor.FromArgb(color.A, color.R, color.G, color.B);
+        }
+
+        public static SDColor WMColorToSDColor(WMColor color)
+        {
+            return SDColor.FromArgb(color.A, color.R, color.G, color.B);
+        }
+    }
+}
--- Unimmersive.Shell/Start/StartScreen.xaml.cs
+++ Unimmersive.Shell/Start/StartScreen.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
-using System.Drawing;
+﻿using Microsoft.WindowsAPICodePack.Shell;
+using System;
+using System.Diagnostics;
 using System.Windows;
+using System.Xml.Linq;
 using Unimmersive.Shell.Start.Controls;
+using Unimmersive.Shell.Methods;
+using System.Windows.Media;
 
 namespace Unimmersive.Shell.Start
 {
@@ -25,11 +29,19 @@ namespace Unimmersive.Shell.Start
         // add tiles when panel is loaded
         private void onPanelLoaded(object sender, RoutedEventArgs e)
         {
-            string[] placeholder = { "squareBracket", "Sangria", "2003page" }; //references to past grkb projects
-            foreach (string name in placeholder)
+            var FOLDERID_AppsFolder = new Guid("{1e87508d-89c2-42f0-8a7e-645a0f50ca58}"); // FIXME: this doesn't work on windows 7 apparantly?
+            ShellObject appsFolder = (ShellObject)KnownFolderHelper.FromKnownFolderId(FOLDERID_AppsFolder);
+            foreach (var app in (IKnownFolder)appsFolder)
             {
                 Tile tile = new Tile();
-                tile.TileName.Content = name;
+                tile.TileIcon.Source = app.Thumbnail.SmallBitmapSource;
+                System.Drawing.Color colorBG = ColorMethods.GetDominantColor(app.Thumbnail.SmallBitmap);
+                System.Drawing.Color colorBR = ColorMethods.ChangeColorBrightness(colorBG, -0.5f);
+                System.Drawing.Color colorFG = ColorMethods.IdealTextColor(colorBG);
+                tile.TileBox.Fill = new SolidColorBrush(ColorMethods.SDColorToWMColor(colorBG));
+                tile.TileBox.Stroke = new SolidColorBrush(ColorMethods.SDColorToWMColor(colorBR));
+                tile.TileName.Foreground = new SolidColorBrush(ColorMethods.SDColorToWMColor(colorFG));
+                tile.TileName.Text = app.Name;
                 TilesPanel.Children.Add(tile);
             }
         }
