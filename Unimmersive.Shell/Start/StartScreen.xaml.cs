c409bc97c14764392e41b221e79aa83e0834f6e8
Chaz "Gamerappa" Peloquin <chazpelo2@gmail.com>
2022-08-20 17:17:17 -0400

    Tiles

--- Unimmersive.Shell/Start/StartScreen.xaml.cs
+++ Unimmersive.Shell/Start/StartScreen.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using Unimmersive.Shell.Start.Controls;
 
 namespace Unimmersive.Shell.Start
 {
@@ -17,6 +20,18 @@ namespace Unimmersive.Shell.Start
         {
             e.Cancel = true;
             return;
+        }
+
+        // add tiles when panel is loaded
+        private void onPanelLoaded(object sender, RoutedEventArgs e)
+        {
+            string[] placeholder = { "squareBracket", "Sangria", "2003page" }; //references to past grkb projects
+            foreach (string name in placeholder)
+            {
+                Tile tile = new Tile();
+                tile.TileName.Content = name;
+                TilesPanel.Children.Add(tile);
+            }
         }
     }
 }
